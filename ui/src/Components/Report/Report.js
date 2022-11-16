9e0e6376e05662942921254413b8b113d198a516
pulivarhimallika <mallikapulivarthi2000@gmail.com>
2022-11-16 12:51:55 +0530

    validations

--- ui/src/Components/Report/Report.js
+++ ui/src/Components/Report/Report.js
@@ -1,3 +1,6 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+// import { object } from "yup/lib/locale";
 function Report() {
   return (
     <div>
@@ -13,40 +16,102 @@ function Report() {
           </div>
         </div>
       </div>
-      <div className="card m-4 shadow ">
-        <div class="card-header">
-          <h5>Payslip Details</h5>
-        </div>
-        <div className="card-body pay  ">
-          <div className="d-flex  justify-content-evenly">
-            <div className="col-5 m-2 "> 
-              <input className="form-control p-3 " placeholder="Company Name" type="text"/>
+      <Formik
+        initialValues={{
+          CompanyName: "",
+          EmployeeNo: "",
+          Dom: "",
+          NoOfWorkingDays: "",
+          GrossSalary: "",
+        }}
+        validationSchema={Yup.object({
+          CompanyName: Yup.string().trim().required("Companyname is required"),
+          EmployeeNo: Yup.string().trim().required("EmployeeNo is required"),
+          Dom: Yup.string().trim().required("date of month/year is required"),
+          NoOfWorkingDays: Yup.string()
+            .trim()
+            .required("No Of Working Days is required"),
+          GrossSalary: Yup.string().trim().required("Gross Salary is required"),
+        })}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        <Form>
+          <div className="card m-4 shadow ">
+            <div class="card-header">
+              <h5>Payslip Details</h5>
             </div>
-            <div className="col-5  m-2">
-              <input className="form-control p-3" placeholder="Employee No" type="text" />
+            <div className="card-body pay  ">
+              <div className="d-flex  justify-content-evenly">
+                <div className="col-5 m-2 ">
+                  <Field
+                    className="form-control p-3 "
+                    placeholder="Company Name"
+                    type="text"
+                    name="CompanyName"
+                  />
+                  <div className="text-danger">
+                    <ErrorMessage name="CompanyName" />
+                  </div>
+                </div>
+                <div className="col-5  m-2">
+                  <Field
+                    className="form-control p-3"
+                    placeholder="Employee No"
+                    type="text"
+                    name="EmployeeNo"
+                  />
+                  <div className="text-danger">
+                    <ErrorMessage name="EmployeeNo" />
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex justify-content-evenly">
+                <div className="col-5  m-2">
+                  <Field
+                    className="form-control p-3"
+                    placeholder="Date Of Month /year"
+                    type="date"
+                    name="Dom"
+                  />
+                  <div className="text-danger">
+                    <ErrorMessage name="Dom" />
+                  </div>
+                </div>
+                <div className="col-5  m-2">
+                  <Field
+                    className="form-control p-3"
+                    placeholder="Number Of Working Days"
+                    type="text"
+                    name="NoOfWorkingDays"
+                  />
+                  <div className="text-danger">
+                    <ErrorMessage name="NoOfWorkingDays" />
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex justify-content-evenly">
+                <div className="col-5  m-2">
+                  <Field
+                    className="form-control p-3"
+                    placeholder="Gross Salary"
+                    type="text"
+                    name="GrossSalary"
+                  />
+                  <div className="text-danger">
+                    <ErrorMessage name="GrossSalary" />
+                  </div>
+                </div>
+                <div className="col-5  m-2"></div>
+              </div>
             </div>
           </div>
-          <div className="d-flex justify-content-evenly">
-            <div className="col-5  m-2">
-              <input className="form-control p-3" placeholder="Date Of Month /year" type="date"/>
-            </div>
-            <div className="col-5  m-2">
-              <input className="form-control p-3" placeholder="Number Of Working Days" type="text"/>
-            </div>
+          <div className=" ms-4 mb-2">
+            <button className="btn btn-primary btn-lg ">Get Payslip</button>
           </div>
-          <div className="d-flex justify-content-evenly">
-            <div className="col-5  m-2">
-              <input className="form-control p-3" placeholder="Gross Salary" type="text"/>
-            </div>
-            <div className="col-5  m-2">
-             
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className=" ms-4 ">
-          <button className="btn btn-primary btn-lg ">Get Payslip</button>
-        </div>
+        </Form>
+      </Formik>
     </div>
   );
 }
