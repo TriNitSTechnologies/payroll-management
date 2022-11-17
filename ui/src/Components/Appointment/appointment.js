78034250590cf67e767818f53db46ae512669fd2
Arun-kumar-trinits <arun.ui.0035@gmail.com>
2022-11-17 18:12:27 +0530

    changes error messages

--- ui/src/Components/Appointment/appointment.js
+++ ui/src/Components/Appointment/appointment.js
@@ -1,45 +1,46 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
-import "./appointment.css"
+import "./appointment.css";
 
 export default function Appointment() {
   return (
     <Formik
       initialValues={{
-        CompanyName: "",
-        CompanyAddress: "",
-        CurrentDate: "",
-        EmployeeName: "",
-        JobTitle: "",
-        AnnualCTC: "",
-        ManagerName: "",
-        EmployeeJoiningDate: "",
-        AcceptanceLastDate: "",
-        HRName: "",
+        companyName: "",
+        companyAddress: "",
+        currentDate: "",
+        employeeName: "",
+        jobTitle: "",
+        annualCTC: "",
+        managerName: "",
+        employeeJoiningDate: "",
+        acceptanceLastDate: "",
+        hrName: "",
       }}
       validationSchema={Yup.object({
-        CompanyName: Yup.string().required("Company Name is requried"),
-        CompanyAddress: Yup.string().required("Company Address is requried"),
-        CurrentDate: Yup.string().required("Current Date is requried"),
-        EmployeeName: Yup.string().required("Employee Name is requried"),
-        JobTitle: Yup.string().required("Job Title is requried"),
-        AnnualCTC: Yup.string().required("Annual CTC is requried"),
-        ManagerName: Yup.string().required("Manager Name is requried"),
-        EmployeeJoiningDate: Yup.string().required(
-          "Employee Joining Date  is requried"),
-        AcceptanceLastDate: Yup.string().required(" Requried"),
-        HRName: Yup.string().required("HR Name is requried"),
+        companyName: Yup.string().required("Company Name is requried"),
+        companyAddress: Yup.string().required("Company Address is requried"),
+        currentDate: Yup.string().required("Current Date is requried"),
+        employeeName: Yup.string().required("Employee Name is requried"),
+        jobTitle: Yup.string().required("Job Title is requried"),
+        annualCTC: Yup.string().required("Annual CTC is requried"),
+        managerName: Yup.string().required("Manager Name is requried"),
+        employeeJoiningDate: Yup.string().required(
+          "Employee Joining Date  is requried"
+        ),
+        acceptanceLastDate: Yup.string().required("Acceptance date is Requried"),
+        hrName: Yup.string().required("HR Name is requried"),
       })}
       onSubmit={(values) => {
         alert(JSON.stringify(values));
-      }}  
+      }}
     >
       <div>
         <Form>
           <div className="card m-4 pay shadow ">
             <div className="card-body d-flex justify-content-between ">
               <div>
-                <small className="fs-6">placeholder</small>
+                <small className="fs-6">Home </small>
               </div>
               <div>
                 <h5>Appointment Letter</h5>
@@ -48,171 +49,157 @@ export default function Appointment() {
           </div>
           <div className="card m-4 shadow ">
             <div className="card-header">
-              <h5>Appointment Letter</h5>
+              <h5>Appointment letter details</h5>
             </div>
-            <div className="card-body pay  ">
+            <div className="card-body pay">
               <div className="d-flex  justify-content-evenly">
                 <div className="col-5 m-2">
                   <Field
                     className="form-control p-3  "
-                    name="CompanyName"
-                    placeholder="Company Name"
+                    name="companyName"
+                    placeholder="Company name"
                     type="text"
                   />
 
                   <small className="text-danger">
-                   
-                    <ErrorMessage name="CompanyName" />
+                    <ErrorMessage name="companyName" />
                   </small>
                 </div>
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="CompanyAddress"
-                    placeholder="Company Address"
+                    name="companyAddress"
+                    placeholder="Company address"
                     input="text"
                   />
                   <small className="text-danger">
-                   
-                   <ErrorMessage name="CompanyAddress" />
-                 </small>
+                    <ErrorMessage name="companyAddress" />
+                  </small>
                 </div>
               </div>
               <div className="d-flex justify-content-evenly">
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="CurrentDate"
-                    placeholder="Current Date"
+                    name="currentDate"
+                    placeholder="Current date"
                     type="text"
                   />
                   <small className="text-danger">
-                   
-                   <ErrorMessage name="CurrentDate" />
-                 </small>
+                    <ErrorMessage name="currentDate" />
+                  </small>
                 </div>
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="EmployeeName"
-                    placeholder="Employee Name"
-                    type="text" 
+                    name="employeeName"
+                    placeholder="Employee name"
+                    type="text"
                   />
                   <small className="text-danger">
-                   
-                   <ErrorMessage name="EmployeeName" />
-                 </small>
+                    <ErrorMessage name="employeeName" />
+                  </small>
                 </div>
               </div>
               <div className="d-flex justify-content-evenly">
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="JobTitle"
-                    placeholder="Job Title"
+                    name="jobTitle"
+                    placeholder="Job title"
                     type="text"
                   />
                   <small className="text-danger">
-                   
-                   <ErrorMessage name="JobTitle" />
-                 </small>
+                    <ErrorMessage name="jobTitle" />
+                  </small>
                 </div>
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="AnnualCTC"
+                    name="annualCTC"
                     placeholder="Annual CTC"
                     type="text"
                   />
-                   <small className="text-danger">
-                   
-                   <ErrorMessage name="AnnualCTC" />
-                 </small>
+                  <small className="text-danger">
+                    <ErrorMessage name="annualCTC" />
+                  </small>
                 </div>
               </div>
               <div className="d-flex justify-content-evenly">
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="ManagerName"
-                    placeholder="Manager Name"
+                    name="managerName"
+                    placeholder="Manager name"
                     type="text"
-                    
                   />
-                   <small className="text-danger">
-                   
-                   <ErrorMessage name="ManagerName" />
-                 </small>
+                  <small className="text-danger">
+                    <ErrorMessage name="managerName" />
+                  </small>
                 </div>
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="EmployeeJoiningDate"
-                    placeholder="Employee Joining Date"
+                    name="employeeJoiningDate"
+                    placeholder="Employee joining date"
                     type="text"
                   />
                   <small className="text-danger">
-                   
-                   <ErrorMessage name="EmployeeJoiningDate" />
-                 </small>
+                    <ErrorMessage name="employeeJoiningDate" />
+                  </small>
                 </div>
               </div>
               <div className="d-flex justify-content-evenly">
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="AcceptanceLastDate"
-                    placeholder="Acceptance Last Date"
+                    name="acceptanceLastDate"
+                    placeholder="Acceptance last date"
                     type="text"
                   />
                   <small className="text-danger">
-                   
-                   <ErrorMessage name="AcceptanceLastDate" />
-                 </small>
+                    <ErrorMessage name="acceptanceLastDate" />
+                  </small>
                 </div>
                 <div className="col-5  m-2">
                   <Field
                     className="form-control p-3"
-                    name="HRName"
-                    placeholder="HR Name"
+                    name="hrName"
+                    placeholder="HR name"
                     type="text"
                   />
-                   <small className="text-danger">
-                   
-                   <ErrorMessage name="HRName" />
-                 </small>
+                  <small className="text-danger">
+                    <ErrorMessage name="hrName" />
+                  </small>
                 </div>
               </div>
 
               <div className="d-flex justify-content-evenly">
                 <div className="col-5  ">
-                 
-              <div className="form-check  ">
-                <input
-                  className="form-check-input "
-                  type="checkbox"
-               
-                  
-                />
-                <label className="form-check-label  text-primary text-opacity-75">
-                  Include Xpayroll generated salary structure from annual CTC
-                </label>
-              </div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value=""
+                      id="flexCheckChecked"
+                    />
+                    <label
+                      className="form-check-label "
+                      htmlFor="flexCheckChecked"
+                    >
+                      Include Xpayroll generated salary structure from annual
+                      CTC
+                    </label>
+                  </div>
                 </div>
-                <div className="col-5  m-2">
-                  
-                  
-                  
-                </div>
+                <div className="col-5  m-2"></div>
               </div>
-
             </div>
-            <div className=" ms-4 text-center ">
+          </div>
+
+          <div className=" ms-4  ">
             <button className="btn btn-primary btn-lg ">GENERATE LETTER</button>
           </div>
-          </div>
-
-          
         </Form>
       </div>
     </Formik>
