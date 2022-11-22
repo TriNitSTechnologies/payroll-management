be6a6a47c0cfb6dbea1ad2336033775808fb0371
Chandra Babu <chandrababu17331@gmail.com>
2022-11-22 16:12:56 +0530

    change the payslip4 in back end data add

--- ui/src/Components/Documents/Documents.js
+++ ui/src/Components/Documents/Documents.js
@@ -4,6 +4,7 @@ import Joinletter from "../Offer Letter/offerletter";
 import Payslip1 from "../Payslip1/Payslip1";
 import Payslip2 from "../Payslip2/Payslip2";
 import Payslip3 from "../Payslip3/Payslip3";
+import Payslip4 from "../Payslip4/Payslip4";
 import "./../../App.css";
 export default function Documents() {
     return (
@@ -12,6 +13,7 @@ export default function Documents() {
             <Payslip2 />
             
             <Payslip3 />
+            <Payslip4 />
         
         
 
